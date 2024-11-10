7e4367f33cf798ee8398d57a5752e8529b9ec8dc
Eugene Choi <eugenechoi@Eugenes-MBP.hsd1.va.comcast.net>
2024-11-09 19:23:49 -0500

    Implemented the typewriter effect

--- src/TypewriterEffect.jsx
+++ src/TypewriterEffect.jsx
@@ -1,9 +1,30 @@
+import {useState, useRef} from 'react';
+
 const TypewriterEffect = () => {
+  const [display, setDisplay] = useState("");
+  const intervalRef = useRef(null);
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    if(intervalRef.current) {
+      setDisplay("");
+      clearInterval(intervalRef.current);
+      intervalRef.current = null;
+    }
     const data = new FormData(e.target);
     // TODO Display the text with typewriter effect
     console.log(`The sentence to display is ${data.get("sentence")}`);
+    let i = 0;
+    intervalRef.current = setInterval(() => {
+      if(i === data.get("sentence").length) {
+        clearInterval(intervalRef.current);
+        intervalRef.current = null;
+      } else {
+        setDisplay(data.get("sentence").slice(0,i+1));
+        i += 1
+      }
+      
+    }, 500);
   };
   return (
     <div>
@@ -22,6 +43,7 @@ const TypewriterEffect = () => {
         />
         <button type="submit">Display with typewriter effect</button>
       </form>
+      <p>{display}</p>
     </div>
   );
 };
